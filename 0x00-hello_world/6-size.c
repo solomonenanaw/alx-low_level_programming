f8fc1c13ae2a9e749e098e59d41db5fcd338d309
solomonenanaw <sekewbeg@gmail.com>
2022-04-08 09:13:58 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,9 @@ char d;
 float f;
 
 printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %1u byte(s)\n", (unsigned long(sizeof(b));
 printf("Size of a long long int: %1u byte(s)\n", (unsigned long(sizeof(c));
-printf("Size of a long float: %1u byte(s)\n", (unsigned long(sizeof(f));    	return (0);
+printf("Size of a float: %1u byte(s)\n", (unsigned long(sizeof(f));    	
+return (0);
 }
