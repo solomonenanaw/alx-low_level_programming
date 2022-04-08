f0170aecf7180ef0df4512aa0036b4325f7e62db
solomonenanaw <sekewbeg@gmail.com>
2022-04-08 13:21:04 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ else if (lastdigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
-else if (lastdigit < 6 && lastdigit != 0) 
+else if (lastdigit < 6 && lastdigit!= 0) 
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
