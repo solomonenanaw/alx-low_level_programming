32177416274c4ee1de28cc87632252bd6ee9634c
solomonenanaw <sekewbeg@gmail.com>
2022-04-08 01:49:42 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+	int a;
+
+	long int b;
+
+	long long int c;
+
+	char d;
+
+	float f;
+
+
+
+	printf("Size of a char:%1u byte(s)\n", (unsigned long)sizeof(d));
+
+	printf("Size of a char:%1u byte(s)\n", (unsigned long)sizeof(a));
+
+	printf("Size of a long int:%1u byte(s)\n", unassigned long(sizeof(b));
+
+			printf("Size of a long long int:%1u byte(s)\n", unassigned long(sizeof(c));
+
+				printf("Size of a long float:%1u byte(s)\n", unassigned long(sizeof(f));
+
+					return (0)
+
+					}
