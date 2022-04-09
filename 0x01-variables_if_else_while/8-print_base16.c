73643acb1f9460402ca100b62bceda3869437766
solomonenanaw <sekewbeg@gmail.com>
2022-04-09 08:10:28 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char a;
+for (a = 1, a < 16 , a++)
+putchar(a);
+return (0);
+}
