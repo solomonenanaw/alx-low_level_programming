89f62956d3ebee949dd241b23e4817f99bd951c3
solomonenanaw <sekewbeg@gmail.com>
2022-04-08 21:37:16 -0700

    capial and small letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char i;
+char I;
+for (i ='a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (I ='A'; I <= 'Z'; I++)
+{
+putchar(I);
+}
+putchar('/n'); 
+
+return (0);
+}
