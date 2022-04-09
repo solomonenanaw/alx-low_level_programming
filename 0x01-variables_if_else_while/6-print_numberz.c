8617711ca2dac335a215e158006ba160ea6a70cb
solomonenanaw <sekewbeg@gmail.com>
2022-04-09 04:47:13 -0700

    no char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
+
+
+int main(void)
+
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
