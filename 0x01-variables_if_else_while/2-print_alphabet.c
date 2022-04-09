cf193cd43b4572704a74db30b68e9a7050866ced
solomonenanaw <sekewbeg@gmail.com>
2022-04-08 21:06:02 -0700

    small letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char i;
+	for ( i= 'a' ; i<= 'z' ; i++)
+	{
+		putchar("%c" , i);
+	}
+	return (0);
+}
