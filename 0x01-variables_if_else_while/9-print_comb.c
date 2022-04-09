c79d15c8ac8e489384bed3660e0738295f76b866
solomonenanaw <sekewbeg@gmail.com>
2022-04-09 09:24:36 -0700

    base16

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 int c;
+
 for (c = '0'; c <= '9'; c++)
 {
 putchar(c);
 if (c != '9')
 {
-										putchar(',');
-										putchar(' ');
+putchar(',');
+putchar(' ');
 }
 }
 putchar('\n');
