703e7da526f8dee465d6a95d755cdbf65813a6c1
solomonenanaw <sekewbeg@gmail.com>
2022-04-08 23:18:06 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+long n = 0;
+int base = 10;
+findNumberOfDigits(n, base);
+n++;
+return (0);
+
+}
