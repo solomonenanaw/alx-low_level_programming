5a3ff23ad8682a54bb54cde91e7480bd42f91195
solomonenanaw <sekewbeg@gmail.com>
2022-04-09 08:36:47 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i;
+for (i= 0; i <= 9; i++)
+{
+putchar("%d", i);
+putchar('\n');
+}
+return (0);
+}
