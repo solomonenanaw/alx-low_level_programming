7eee5b4ae98187b53518e0a7a55b0f6aa8b67296
solomonenanaw <sekewbeg@gmail.com>
2022-04-10 11:02:34 -0700

    comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+int i, j, k;
+int arr[3];
+printf("Enter the first number : ");
+scanf("%d", &arr[0]);
+printf("Enter the second number : ");
+scanf("%d", &arr[1]);
+printf("Enter the third number : ");
+scanf("%d", &arr[2]);
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+for (k = 0; k < 3; k++)
+{
+if (i != j && j != k && k != i)
+{                   
+printf("[%d %d %d]\n", arr[i], arr[j], arr[k]);
+}
+}
+}
+}
+}
