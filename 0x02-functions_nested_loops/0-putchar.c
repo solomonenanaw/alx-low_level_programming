a92bbd8ca1ac34a3176f5ecae6050f7cde8cf27e
solomonenanaw <sekewbeg@gmil.com>
2022-04-12 12:40:40 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
  * main-main block
- * descriptin: prints list of value
- *
- * Return - successprogram returns zero.
+ * descriptin: prints -putchar in new line
+ * Return:this program program returns zero.
  */
 int main(void)
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u'); 
@@ -17,8 +14,6 @@ _putchar('c');
 _putchar('h'); 
 _putchar('a'); 
 _putchar('r'); 
-
 _putchar('\n');
-
 return (0);
 }
