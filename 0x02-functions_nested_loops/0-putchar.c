c7510feae4ff2ff68c525852f440b23d710b0d90
solomonenanaw <sekewbeg@gmail.com>
2022-04-12 10:26:37 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>                                                                                                        
+int main(void)                                                                                                            
+{                                                                                                                         
+_putchar("_putchar\n")   
+return (0); 
+} 
