f794c39bd7119ec701643a43b39c13420f8f1111
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 03:44:32 -0700

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include"main.h"
 #include<stdio.h>
 /**
- * description - prints alphabet
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
 char letter;
 for (letter = 'a' ; letter <= 'z' ; letter++)
-{
 _putchar(letter);
 
 _putchar('\n');
