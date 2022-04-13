5cbaf33a30bf90d8c059e57bf6c76dc62d575796
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 12:08:21 -0700

    f

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - cheeck if the character is lower
+ * @c: the character
+ * Return: 1 if character is lower
+ */
+
+int _isalpha(int c)
+
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
