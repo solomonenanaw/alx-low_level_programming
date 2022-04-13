58b6f9ec17a10641b24d8e165ab48b7c91104c23
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 00:22:31 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,10 +14,8 @@ _putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
-_putchar('h'); 
-_putchar('a'); 
-_putchar('r'); 
-_putchar('\n');
-
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
