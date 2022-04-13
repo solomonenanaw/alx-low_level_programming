d01e387ce6221abc6e093ed1e98714b2b3fb7743
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 12:27:22 -0700

    g

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_sign  - cheeck if the character is lower
+ * @c: the character
+ * Return: one if character is lower
+ */
+
+
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+
+		(
+
+		 _putchar('*');
+
+		 return (1);
+
+		 }
+
+		 else if (n== 0)
+
+		 {
+
+		 _putchar('0');
+
+		 return (0);
+
+		 }
+
+		 else
+
+		 {
+
+			 _putchar('_');
+
+			 return (-1);
+
+		 }
+
+}
