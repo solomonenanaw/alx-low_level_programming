a8abcd40e602a1b684eb4c555c4704923d759c2a
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 04:20:18 -0700

    g

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_alphabet - prints teen times alphabet
+ * print_alphabet_x10 - prints teen times alphabet
  */
-
 void print_alphabet_x10(void)
 {
 char letter;
