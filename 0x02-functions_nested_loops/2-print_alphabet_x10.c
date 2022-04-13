eb0114213d3336925ae4cef9584d7f84277e7e6f
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 11:11:47 -0700

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,9 @@
  */
 void print_alphabet_x10(void)
 {
-int counter = 0;
+int count = 0;
 char letter;
-while (counter++ <= 9)
+while (count++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
