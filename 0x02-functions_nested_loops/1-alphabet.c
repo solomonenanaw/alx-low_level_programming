9c11a1b074e4e83601686002d2361db759fbd310
solomonenanaw <sekewbeg@gmil.com>
2022-04-13 00:44:16 -0700

    v

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * main - entry
+ * description - prints alphabet
+ * return - success
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a' ; letter<= 'z' ; a++)
+{
+_putchar(letter);
+_putchar('\n');
+return (0);
+}
+
