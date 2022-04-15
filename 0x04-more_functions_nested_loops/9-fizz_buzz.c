1f20539c60eae013284ecb5c6318b85efc4cb4f1
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 12:21:14 -0700

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,53 +11,35 @@
 int main(void)
 
 {
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+}
 
-		int i;
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+}
 
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}
 
+else if (i == 1)
+{
+printf("%d", i);
+}
 
-			for (i = 1; i <= 100; i++)
+else
+{
+printf(" %d", i);
+}
 
-					{
-
-								if (i % 3 == 0 && i % 5 != 0)
-
-											{
-
-															printf(" Fizz");
-
-																	} else if (i % 5 == 0 && i % 3 != 0)
-
-																				{
-
-																								printf(" Buzz");
-
-																										} else if (i % 3 == 0 && i % 5 == 0)
-
-																													{
-
-																																	printf(" FizzBuzz");
-
-																																			} else if (i == 1)
-
-																																						{
-
-																																										printf("%d", i);
-
-																																												} else
-
-																																															{
-
-																																																			printf(" %d", i);
-
-																																																					}
-
-																																													}
-
-				printf("\n");
-
-
-
-					return (0);
-
+}
+printf("\n");
+return (0);
 }
