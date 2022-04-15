fc59779c71cb8a8437f8a0b0bdad08f92cf93685
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 14:06:34 -0700

    l

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: zero
+ * Return: 0
  */
 
 int main(void)
@@ -12,12 +12,14 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+
 /**
  *      while (i < 10)
  *      { 
  *      putchar(i);
  *      }
  */
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
