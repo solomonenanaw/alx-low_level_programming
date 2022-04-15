9970ed15df0a6a33bc698779a729d5e741f3a2c5
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 12:25:34 -0700

    l

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,61 +10,31 @@
 int main(void)
 
 {
+long int n;
+long int max;
+long int i;
 
-		long int n;
+n = 612852475143;
+max = -1;
 
-			long int max;
+while (n % 2 == 0)
+{
+max = 2;
+n /= 2;
+}
 
-				long int i;
+for (i = 3; i <= sqrt(n); i = i + 2)
+{
+while (n % i == 0)
+{
+max = i;
+n = n / i;
+}
 
+}
 
-
-					n = 612852475143;
-
-						max = -1;
-
-
-
-							while (n % 2 == 0)
-
-									{
-
-												max = 2;
-
-														n /= 2;
-
-															}
-
-
-
-								for (i = 3; i <= sqrt(n); i = i + 2)
-
-										{
-
-													while (n % i == 0)
-
-																{
-
-																				max = i;
-
-																							n = n / i;
-
-																									}
-
-														}
-
-
-
-									if (n > 2)
-
-												max = n;
-
-
-
-										printf("%ld\n", max);
-
-
-
-											return (0);
-
+if (n > 2)
+max = n;
+printf("%ld\n", max);
+return (0);
 }
