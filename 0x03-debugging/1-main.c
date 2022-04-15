f67fb5e6cffe82c0275961853f714cc87396a8dd
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 13:39:00 -0700

    ok

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,6 +11,7 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
+
 i = 0;
 /**
  *	while (i < 10)
@@ -20,5 +21,6 @@ i = 0;
  */
 
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
