02f303664058a5705b7303a4f04b1b7766820a92
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 11:59:27 -0700

    g

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
+ * print_numbers - prints the numbers, from zero to nine,
  * followed by a new line
  */
 void print_numbers(void)
@@ -13,4 +13,5 @@ for (i = 0; i < 10; i++)
 _putchar(i + '0');
 }
 _putchar('\n');
+
 }
