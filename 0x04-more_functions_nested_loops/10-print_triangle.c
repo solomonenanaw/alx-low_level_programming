3ef8d05c0aafeba2b745d49ff480716e1bf28b0c
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 08:00:07 -0700

    l

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,40 +1,55 @@
-#include <stdio.h>
-#include <math.h>
-
+#include "main.h"
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
- * followed by a new line
- * Return: Always 0 (Success)
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
 
-int main(void)
+ void print_triangle(int size)
+
 {
 
-long int n;
-long int max;
-long int i;
+		if (size <= 0)
 
-n = 612852475143;
-max = -1;
-while (n % 2 == 0)
-{
-max = 2;
-n /= 2;
-}
+				{
 
-for (i = 3; i <= sqrt(n); i = i + 2)
-{
-while (n % i == 0)
-{
-max = i;
-n = n / i;
-}
-}
-if (n > 2)
-max = n;
+							_putchar('\n');
 
-printf("%ld\n", max);
+								} else
 
-return (0);
+										{
+
+													int i, j;
+
+
+
+															for (i = 1; i <= size; i++)
+
+																		{
+
+																						for (j = i; j < size; j++)
+
+																										{
+
+																															_putchar(' ');
+
+																																		}
+
+
+
+																									for (j = 1; j <= i; j++)
+
+																													{
+
+																																		_putchar('#');
+
+																																					}
+
+
+
+																												_putchar('\n');
+
+																														}
+
+																}
 
 }
