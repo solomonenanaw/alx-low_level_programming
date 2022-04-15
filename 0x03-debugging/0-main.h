9f5f6cfc5380f20e715a38b11f3e46355eb43783
solomonenanaw <sekewbeg@gmil.com>
2022-04-15 13:09:20 -0700

    ok

--- 0x03-debugging/0-main.h
+++ 0x03-debugging/0-main.h
@@ -3,11 +3,14 @@
 /**
  * main - tests function that prints if intger is positive or negative                                                                  * Return: zero                                                       
  */    
-int main()                                                                                                                             
+
+int main(void)                                                                                                                             
 { 
 int i;                                                                                                                                 
+
 i = 0;                                                                                                                                 
 positive_or_negative(i);                                                                                                               
+
 return (0);                                                                                                                            
 }
 
