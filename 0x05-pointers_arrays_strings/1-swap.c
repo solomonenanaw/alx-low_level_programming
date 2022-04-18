f868deca539a3dfef0918a67bd7476cd86d68d59
solomonenanaw <sekewbeg@gmil.com>
2022-04-18 11:45:59 -0700

    h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @a: assign value
+ * @b: assign value
+ * Return: Always 0.
+  */
+
+void swap_int(int *a, int *b)
+
+{
+*a = 98;
+*b = 42;
+swap_int(&a, &b);
+}
