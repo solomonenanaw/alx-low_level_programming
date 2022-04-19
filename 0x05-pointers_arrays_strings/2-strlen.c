4177a13e7ed0045dcb12a88b697cfbfc19eaaa30
solomonenanaw <sekewbeg@gmil.com>
2022-04-18 20:44:01 -0700

    correct

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,32 +1,18 @@
 #include "main.h"                                                                                                                     
+/**                                                                             
+ * _strlen - length of the string                                 
+ * @s: returns pointer value                                                    
+ * return : value                                                     
+ */                                                                             
 
-/**                                                                                                                                   
+int _strlen(char *s)                                                           
+{                                                      
+int a;                                                                          
 
- * _strlen - length of the string                                                                                                  
+a = 0;                                                                          while (s[a] != '\0')                                                         
+{                                                                               a++                                                                                                                                   
+}                                                                               
 
- * @s: returns pointer value                                                                                                          
-
- * return : value                                                                                                                     
-
- */                                                                                                                                   
-
-int _strlen(char *s)                                                                                                                  
-
-{                                                                                                                                     
-
-int a;                                                                                                                                
-
-
-
-a = 0;                                                                                                                                
-
-while (s[a] != '\0')                                                                                                                  
-
-{                                                                                                                                     
-
-a++                                                                                                                                   
-
-}                                                                                                                                     
-
-return (a);                                                                                                                             
+return (a);
+}
 
