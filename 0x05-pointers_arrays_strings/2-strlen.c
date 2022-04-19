87b0ec32e6d1ec2d466c25c79206bb676de22986
solomonenanaw <sekewbeg@gmil.com>
2022-04-18 20:46:33 -0700

    correct

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,13 @@
  * @s: returns pointer value                                                    
  * return : value                                                     
  */                                                                             
-
 int _strlen(char *s)                                                           
 {                                                      
 int a;                                                                          
 
 a = 0;                                                                          while (s[a] != '\0')                                                         
-{                                                                               a++                                                                                                                                   
+{
+a++;                                                                                                                                   
 }                                                                               
 
 return (a);
