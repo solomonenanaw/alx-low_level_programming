285164b4025f744658b228cba33e270afe13bc9c
solomonenanaw <sekewbeg@gmil.com>
2022-04-18 17:30:52 -0700

    h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"                                                                                                                     
+
+/**                                                                                                                                   
+ * int_strlen - length of the string                                                                                                  
+ *
+ * @s: returns pointer value                                                                                                          
+ * return : value                                                                                                                     
+ */                                                                                                                                   
+
+int _strlen(char *s)                                                                                                                  
+{                                                                                                                                     
+int a;                                                                                                                                
+
+a = 0;                                                                                                                                
+while (a[0] != '\0')                                                                                                                  
+{                                                                                                                                     
+a++                                                                                                                                   
+}                                                                                                                                     
+return a;                                                                                                                             
+}     
