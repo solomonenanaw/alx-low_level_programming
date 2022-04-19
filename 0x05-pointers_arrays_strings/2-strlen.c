4bad2080c6c91ff4072300b60c73c78d8b108503
solomonenanaw <sekewbeg@gmil.com>
2022-04-18 20:36:49 -0700

    h

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,32 @@
 #include "main.h"                                                                                                                     
-/**
- * _strlen - length of the string 
- * @s : a pointer to an int that will be chaged/update
- * return : void it means the answer is true 
- */                                                                                                                                   
-int _strlen(char *s)                                                            {                                                                              
-int a;     
 
-a = 0;
-while (s[a] != '\0')                  
-{                                                                               a++                                                                                                                                   
+/**                                                                                                                                   
+
+ * _strlen - length of the string                                                                                                  
+
+ * @s: returns pointer value                                                                                                          
+
+ * return : value                                                                                                                     
+
+ */                                                                                                                                   
+
+int _strlen(char *s)                                                                                                                  
+
+{                                                                                                                                     
+
+int a;                                                                                                                                
+
+
+
+a = 0;                                                                                                                                
+
+while (s[a] != '\0')                                                                                                                  
+
+{                                                                                                                                     
+
+a++                                                                                                                                   
+
 }                                                                                                                                     
+
 return (a);                                                                                                                             
-}     
+
