924f30923421c093d195e2087105dc06b3d62005
solomonenanaw <sekewbeg@gmil.com>
2022-04-21 07:10:55 -0700

    k

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
+#include "main.h"
 /**
  * strcat the function that concatenete two strings
  * @des pointer to the destination input
  * @src pointer to the destination input
  * Return a pointer string values of @dest 
  */
-#include "main.h"
-
 char *_strcat(char *dest, char *src)
 
 {
