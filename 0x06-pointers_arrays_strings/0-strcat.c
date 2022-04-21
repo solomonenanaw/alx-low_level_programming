e7f225cb78857d1081cd7e849931834359cdc6b1
solomonenanaw <sekewbeg@gmil.com>
2022-04-21 10:11:55 -0700

    g

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,13 @@
  */
 
 char *_strcat(char *dest, char *src)
-int x, y;
-
-for (x = 0; dest[x] != '\0'; x++)
-
-for (y = 0; src[y] != '\0'; y++)
 {
-dest[x] = src[y];
-x++;
-}
-dest[x] = '\0';
+int a,b;
+
+a = 0;
+while (dest[a])
+a++
+for (b = 0; src[b]; b++)
+dest[a++] = src[b];
 return (dest);
 }
