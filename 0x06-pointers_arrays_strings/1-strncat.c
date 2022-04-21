76012456192f9b97a0d1c2dcbda4a8b852f7bf28
solomonenanaw <sekewbeg@gmil.com>
2022-04-21 11:45:26 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,13 @@
 /**
  * *_strncat - like _strcat but it will use n bytes from src
+ *
  * @dest: string to be concatenated
  * @src: string to be concatenated until n limit
  * @n: number of bytes that will be used from src
+ *
  * Return: pointer to the resulting string dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 int c,i;
