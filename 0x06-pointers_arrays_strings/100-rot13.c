175ddc0bea70cfd20cdbb5a0b77f81eeb1d710ac
solomonenanaw <sekewbeg@gmil.com>
2022-04-22 09:23:47 -0700

    j

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,19 @@
 #include "main.h"
+
 /**
  ** rot13 - function that encodes string using rot13
  * @s: string
  * Return: encoded string
  */
+
 char *rot13(char *s)
 {
 int x, y;
 
-char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char input[] = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = 
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (x = 0; s[x] != '\0'; x++)
 {
