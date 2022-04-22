147faa10016bb2bc2229599b056bbd5278956c93
solomonenanaw <sekewbeg@gmil.com>
2022-04-22 09:05:04 -0700

    j

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
-/* cap_string - a function that capitalize
- *       all words of a string
+/**
+ * cap_string - a function that capitalizes
+ *      all words of a string
  *
  * @s: pointer to char input array
  *
@@ -24,13 +25,17 @@ if (i == 0)
 {
 s[i] -= 32;
 }
-/*
+/**
  * if we find any character matching the below before any small
  * letter we change that value to a capital letter.
  */
-if (s[i - 1] == 32  s[i - 1] == 9  s[i - 1] == 10  s[i - 1] == 44  s[i - 1] == 59  s[i - 1] == 40  s[i - 1] == 33  s[i - 1] == 63  s[i - 1] == 34  s[i - 1] == 40  s[i - 1] == 41  s[i - 1] == 123  s[i - 1] == 124)
+
+if (s[i - 1] == 32  s[i - 1] == 9  s[i - 1] == 10  s[i - 1] == 44 
+s[i - 1] == 59  s[i - 1] == 46  s[i - 1] == 33  s[i - 1] == 63
+s[i - 1] == 34  s[i - 1] == 40  s[i - 1] == 41  s[i - 1] == 123 
+s[i - 1] == 124)
 {
-s[i] -= 32;
+	s[i] -= 32;
 }
 }
 i++;
