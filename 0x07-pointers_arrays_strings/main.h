25c0580740f8d0122c85a8abd04b81dd943a3752
solomonenanaw <sekewbeg@gmil.com>
2022-04-25 11:55:29 -0700

    g

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
-#define MAIN-H
+#define MAIN_H
 
-#include <stdio.h>
+int _putchar(char);
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,6 +11,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-
+void set_string(char **s, char *to);
 
 #endif /* MAIN.H */
