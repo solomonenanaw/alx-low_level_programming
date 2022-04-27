192bdcd127f0fbf0c99aa3bf09af9395b7fb1a98
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 16:57:31 -0700

    f

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
  * is_prime_number  -  fuction to find prime
  * @n: number
  * Return: 0 if not prime, 1 if prime
@@ -15,9 +14,9 @@ if (i <= n)
 {
 if (n % i == 0)
 {
-check ++;
+check = check + 1;
 }
-i ++;
+i = i + 1;
 }
 if (check == 2)
 {
