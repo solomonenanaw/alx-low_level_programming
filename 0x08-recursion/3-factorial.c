261636faa1566fc589c0febafd2c5835c7b0ae1e
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 11:56:38 -0700

    task3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - function to return factorial
+ * @n: number
+ * Return: value
+ */
+int factorial(int n)
+{
+if ( n < 0)
+return (-1);
+else if (n == 0 || n == 1)
+return (1);
+else
+return (n *factorial(n-1));
+}
