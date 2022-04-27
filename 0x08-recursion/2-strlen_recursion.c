a4b5ea613986223d4c3afde5f1bb48c7dbc5f502
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 10:34:29 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,8 @@
 
 int _strlen_recursion(char *s)
 {
-int a = 0;
-
-if (*s != '/0')
-a = 1 + _strlen_recursion(s + 1)
-return (a);
+if (*s != '\0')
+return 1 + _strlen_recursion(s + 1);
 else 
 return (0);
 }
