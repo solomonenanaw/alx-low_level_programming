5e400291da12a19ecd6645cec498404d7f744a93
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 13:03:45 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,10 +10,10 @@ int _pow_recursion(int x, int y)
 {
 if (y < 0)
 return (-1);
-else if(y == 0)
+else if (y == 0)
 return (1);
 else if (y == 1)
 return (x);
 else
-return (x * _pow_recursion(x * x, (y - 1)/2));
+return (x * _pow_recursion(x * x, (y - 1) / 2));
 }
