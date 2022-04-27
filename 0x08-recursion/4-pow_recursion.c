5c5f021c183b28dba70adc1c2f9da858946bf183
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 12:16:05 -0700

    g

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - function returns rased power
+ * @x: vaue
+ * @y: value
+ * Return: value of exponent
+ */
+int _pow_recursion(int x, int y)
+{
+int z;
+
+if (y < 0)
+return (-1);
+else
+z = _pow_recursion(x,y)
+return (z);
+}
