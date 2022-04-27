11fa65900471e6d9059450d91617b75d3397d048
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 05:42:03 -0700

    g

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H                                                                                                                          
                                                                                                                                         
 
-int _putchar(char);                                                                                                                     
-
-
-void _puts_recursion(char *s);
+int _putchar(char c);                                                                                                                   void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
