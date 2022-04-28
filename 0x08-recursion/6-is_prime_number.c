9b79ed95c108fef6b09339fe89a2356a5cfe5939
solomonenanaw <sekewbeg@gmil.com>
2022-04-27 17:13:08 -0700

    g

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,7 @@
  * @n: number
  * Return: 0 if not prime, 1 if prime
  */
-/*int is_prime_number(int n)
+int is_prime_number(int n)
 {
 int i = 1;
 
@@ -27,45 +27,4 @@ else
 return (0);
 }
 }
-*/
-
-int is_prime_number(int n) 
-{
-int prime;
-
-prime = is_prime_number(n, n / 2);
-if (prime ==1 )
-return (1);
-else
-return (0);
-}
-int is_prime_number(int n, int i)
-{
-if (i == 1)
-{
-return (1);
-}
-else
-{
-if (n % i == 0)
-return (0);
-else
-is_prime_number(n, i - 1);
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
