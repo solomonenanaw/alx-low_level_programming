718b64dfbb27e578191cd47ad4bcbbb3d5b94f57
solomonenanaw <sekewbeg@gmail.com>
2022-04-30 02:11:03 -0700

    g

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-
-=======
->>>>>>> 15a9a8040da5ce10cfcfc4d72853baca9b36f375
 /**
  * main - Entry ppoint
  * @argc: size of argv array
  * @argv: array of size argc
  * Return: Always 0 Sucess
  */
-<<<<<<< HEAD
-
-int main (int argc,char *argv[])
-{
-
-int i;
-
-=======
-int main (int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
->>>>>>> 15a9a8040da5ce10cfcfc4d72853baca9b36f375
-for ( i = 0; i < argc; i++)
+
+for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[0]);
 }
