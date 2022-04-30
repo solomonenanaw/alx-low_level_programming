909ce15dbce04ea3c88b5d01d44931fd8cb31745
solomonenanaw <sekewbeg@gmail.com>
2022-04-30 10:58:06 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,12 +8,21 @@
  */
 
 int main(int argc, char *argv[])
-{
-int i;
 
-for (i = 0; i < argc; i++)
 {
-printf("%s\n", argv[0]);
-}
-return (0);
+
+	int i;
+
+
+
+	for (i = 0; i < argc; i++)
+
+	{
+
+		printf("%s\n", argv[0]);
+
+	}
+
+	return (0);
+
 }
