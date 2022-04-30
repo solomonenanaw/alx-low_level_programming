a2910fc086f175300f9302d045d123879f984dc4
solomonenanaw <sekewbeg@gmil.com>
2022-04-30 01:53:21 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry ppoint
+ * @argc: size of argv array
+ * @argv: array of size argc
+ * Return: Always 0 Sucess
+ */
+
+int main (int argc,char *argv[])
+{
+
+int i;
+
+for ( i = 0; i < argc; i++)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
