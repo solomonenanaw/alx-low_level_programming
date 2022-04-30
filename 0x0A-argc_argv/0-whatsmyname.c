00c4cabacf7c23e23aada82a8b4aab6eb13327a6
solomonenanaw <sekewbeg@gmail.com>
2022-04-30 04:08:51 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - Entry ppoint
- * @argc: size of argv array
- * @argv: array of size argc
+ * main - program that prints its name
+ * @argc: num of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
  * Return: Always 0 Sucess
  */
+
 int main(int argc, char *argv[])
 {
-int i;
-
-for (i = 0; i < argc; i++)
-{
 printf("%s\n", argv[0]);
-}
 return (0);
 }
