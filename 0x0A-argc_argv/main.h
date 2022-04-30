9e50f62db5001b391ff02163db43def0e99bbde9
solomonenanaw <sekewbeg@gmail.com>
2022-04-30 23:16:39 +0000

    g

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,9 +3,4 @@
 int _putchar(char c);
 int main(int argc, char* argv[]);
 int main(int argc);
-#endif#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-int main(int argc, char* argv[]);
-int main(int argc);
 #endif
