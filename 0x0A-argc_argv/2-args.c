acadab190b29f684fc5ba840deae42e84da18a58
solomonenanaw <sekewbeg@gmail.com>
2022-04-30 23:29:55 +0000

    g

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - is a function that prints all the arguments
+ * @argc: is the arguments
+ * @argv: is the pointer to the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}#include<stdio.h>
+/**
+ * main - is a function that prints all the arguments
+ * @argc: is the arguments
+ * @argv: is the pointer to the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
