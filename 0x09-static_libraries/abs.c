65292fe5cb3fb2e88428e760b960749c6bc781d9
solomonenanaw <sekewbeg@gmail.com>
2022-05-01 01:28:25 +0000

    g

---
+++ 0x09-static_libraries/abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - cheeck if the character is lower
+ * @n: the character
+ * Return: nif character is lower
+ */
+
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
