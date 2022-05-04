b0ef8a954100ac7dd5ef0fc0278d294118083eae
solomonenanaw <sekewbeg@gmail.com>
2022-05-04 23:53:10 +0000

    g

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,16 +7,19 @@
  * @size: size of the array
  * Return: Null or pointer to the array
  */
+
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
+
 char *s;
+
 if (size == 0)
 return (Null);
 s = (char *)malloc(size * sizeof(char));
 if (s == 0)
 return (Null);
 for (i = 0; i < size; i++)
-S[i] = c;
-return (c);
+s[i] = c;
+return (s);
 }
