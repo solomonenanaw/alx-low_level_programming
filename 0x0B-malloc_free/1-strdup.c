4017fa56a2b409f26e004bd9fb3bb2dad3fda885
solomonenanaw <sekewbeg@gmail.com>
2022-05-05 00:20:07 +0000

    f

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+char *_strdup(char *str)
+{
+char str;
+Char s;
+if (str == NULL)
+return (NULL);
+s = (char *)malloc(str(char*));
+if (s == NULL)
+{
+return (Null);
+}
+else
+{
+return (s);
+}
+free (s);
+}
