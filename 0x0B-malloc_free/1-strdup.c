58234bd6d19e28c52b5e59fcdbb495a7e2ae385a
solomonenanaw <sekewbeg@gmail.com>
2022-05-05 00:53:40 +0000

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_strdup - function returns a pointer to a new string 
- *@str: string to duplicate
- *Return: NuLL for error copy string if success
+ * _strdup - function returns a pointer to a new string 
+ * @str: string to duplicate
+ * 
+ * Return: NuLL for error copy string if success
  */
 char *_strdup(char *str)
 {
@@ -15,7 +16,8 @@ char *ptr;
 if (!str)
 return (NULL);
 for (len = 0; str[len]; len++)
-ptr = (char *)malloc(len * sizeof(char));
+;
+ptr = (char *)malloc(len * sizeof(char) + 1);
 if (!ptr)
 return (NULL);
 for (a = 0; a <= len ; a++)
