c5e1ad413eb97e81ed1cd402705fb5b2c180bc47
solomonenanaw <sekewbeg@gmail.com>
2022-05-06 12:59:33 +0000

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_checked - Returns a pointer to the allocated memory
+ *@b: assigned value 
+ * 
+ * Return: success return pointer fail 98
+ */
+void *malloc_checked(unsigned int b)
+{
+int *i;
+unsigned int b;
+i = (int *) malloc(98 *sizeof(int));
+if (malloc == NULL)
+{
+return (98);
+}
+else
+b == i;
+return (b);
+}
