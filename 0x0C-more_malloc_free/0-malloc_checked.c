a7a23ac218d1c3e16ca5b7a39bf948bc0c5403c4
solomonenanaw <sekewbeg@gmail.com>
2022-05-06 16:49:39 +0000

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * *malloc_checked - function to check malloc 
- * @b: size of malloc
- * 
+ * malloc_checked - does what malloc does and checks if null
+ * @b: size to alloc for malloc
+ *
  * Return: void pointer to allocated mem
  */
-
 void *malloc_checked(unsigned int b)
 {
-void *i;
+	void *a;
 
-i = malloc(b);
-if (i == NULL)
-exit(98);
-return (i);
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
 }
-
