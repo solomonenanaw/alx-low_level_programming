d96d223f5cfecc0f90b9e8eb3a859e43bcb80cd4
solomonenanaw <sekewbeg@gmail.com>
2022-05-13 08:34:07 +0000

    g

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct funckey - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @f: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
 
 #endif 
