50493d32fe4961af8a34a5ec4db0f0286072795c
solomonenanaw <sekewbeg@gmail.com>
2022-05-13 20:28:49 +0000

    task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,24 +1,26 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 
 int _putchar(char c);
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct funckey - a key for picking functions to use for printing
- * in 3-print_all.c
- *
- * @f: function to use for printing
- * @spec: character specifier
- */
-typedef struct funckey
-{
-	void (*f)(va_list);
-	char spec;
-} funckey;
 
-#endif 
+
+/**
+ * struct format_struct - format-type class
+ * @format: format identifier
+ * @fmt_print_func: pointer to function that prints in the format of @format
+ */
+
+struct format_struct
+{
+	char format;
+	void (*fmt_print_func)(va_list *arg_list);
+};
+
+typedef struct format_struct format_struct_ptr;
+
+#endif
