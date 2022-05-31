83136293ef5023bbcce2b11086624a5dcf95a797
solomonenanaw <sekewbeg@gmail.com>
2022-05-30 17:10:03 -0700

    f

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -21,7 +21,7 @@ void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic numbe * rs.
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
@@ -44,7 +44,7 @@ exit(98);
 
 /**
  * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic numbe * rs.
  * Description: Magic numbers are separated by spaces.
  */
 void print_magic(unsigned char *e_ident)
@@ -119,6 +119,7 @@ void print_version(unsigned char *e_ident)
 {
 printf("  Version:                           %d",
 e_ident[EI_VERSION]);
+
 switch (e_ident[EI_VERSION])
 {
 case EV_CURRENT:
@@ -171,7 +172,9 @@ break;
 case ELFOSABI_STANDALONE:
 printf("Standalone App\n");
 break;
+
 default:
+
 printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 }
 }
@@ -253,7 +256,7 @@ void close_elf(int elf)
 if (close(elf) == -1)
 {
 dprintf(STDERR_FILENO,
-Error: Can't close fd %d\n", elf);
+"Error: Can't close fd %d\n", elf);
 exit(98);
 }
 }
@@ -305,6 +308,7 @@ print_osabi(header->e_ident);
 print_abi(header->e_ident);
 print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
+
 free(header);
 close_elf(o);
 return (0);
