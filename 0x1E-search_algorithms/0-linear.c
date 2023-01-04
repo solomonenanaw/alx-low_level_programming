98354067bb8f5dc55a9deffb796bd458c79700ed
solomonenanaw <sekewbeg@gmail.com>
2023-01-04 11:19:28 +0000

    to fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,6 +19,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
+
 	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
